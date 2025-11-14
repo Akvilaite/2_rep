a323a85c9268a17c7bcda1395c701843d268a92a
Akvilaite <akvile.beinoriute@gmail.com>
2025-11-14 08:10:38 +0200

    destruktoriu tvarkymas

--- 3 strategija/studentas.cpp
+++ 3 strategija/studentas.cpp
@@ -17,7 +17,7 @@ void Studentas::computeGalutiniai() {
     double vid = sum / paz_.size();
     galVid_ = vid * 0.4 + egz_ * 0.6;
 
-    // mediana
+   
     vector<int> tmp = paz_;
     sort(tmp.begin(), tmp.end());
     double med = 0.0;
