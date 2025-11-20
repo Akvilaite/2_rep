ca59a7db68dd65aa9b577fc19195e452b9913351
Akvilaite <akvile.beinoriute@gmail.com>
2025-11-20 14:43:08 +0200

    pakeitimai ivesties

--- 3 strategija/studentas.cpp
+++ 3 strategija/studentas.cpp
@@ -3,104 +3,163 @@
 #include <algorithm>
 #include <limits>
 #include <sstream>
-using namespace std;
 
+using namespace std;
 
 void Studentas::computeGalutiniai() {
     if (paz_.empty()) {
-        galVid_ = 0.0;
-        galMed_ = 0.0;
+        galVid_ = egz_;
+        galMed_ = egz_;
         return;
     }
-    double sum = 0.0;
-    for (int x : paz_) sum += x;
-    double vid = sum / paz_.size();
+
+    double suma = 0.0;
+    for (int x : paz_) suma += x;
+    double vid = suma / paz_.size();
     galVid_ = vid * 0.4 + egz_ * 0.6;
 
-   
     vector<int> tmp = paz_;
     sort(tmp.begin(), tmp.end());
+
     double med = 0.0;
     size_t n = tmp.size();
-    if (n % 2 == 0) med = (tmp[n / 2 - 1] + tmp[n / 2]) / 2.0;
-    else med = tmp[n / 2];
+
+    if (n % 2 == 0)
+        med = (tmp[n / 2 - 1] + tmp[n / 2]) / 2.0;
+    else
+        med = tmp[n / 2];
+
     galMed_ = med * 0.4 + egz_ * 0.6;
 }
 
 std::istream& Studentas::readStudent(std::istream& is) {
     is >> var_ >> pav_;
     paz_.clear();
-    int pazymys;
-    while (is >> pazymys) {
-        if (pazymys >= 0 && pazymys <= 10) {
-            paz_.push_back(pazymys);
+
+    string token;
+    vector<int> numbers;
+
+    while (is >> token) {
+        try {
+            int x = stoi(token);
+            numbers.push_back(x);
         }
-        else {
-            break;  
+        catch (...) {
+            break;
         }
     }
 
-    if (!paz_.empty()) {
-        egz_ = paz_.back();
-        paz_.pop_back();
+    if (!numbers.empty()) {
+        egz_ = numbers.back();
+        numbers.pop_back();
+        paz_ = numbers;
     }
 
     computeGalutiniai();
     return is;
 }
 
-
 std::istream& operator>>(std::istream& is, Studentas& s) {
     return s.readStudent(is);
 }
 
 std::ostream& operator<<(std::ostream& os, const Studentas& s) {
-    os << s.var() << " " << s.pav();
+    os << s.var() << " " << s.pav() << " | ND: ";
 
-    if (!s.paz().empty()) {
-        os << " Pazymiai: ";
-        for (size_t i = 0; i < s.paz().size(); ++i) {
-            os << s.paz()[i];
-            if (i < s.paz().size() - 1) os << ", ";
-        }
+    for (size_t i = 0; i < s.paz().size(); ++i) {
+        os << s.paz()[i];
+        if (i < s.paz().size() - 1) os << ", ";
     }
 
-    os << " Egz: " << s.egz();
-    os << " | Gal.(Vid.): " << fixed << setprecision(2) << s.galVid();
-    os << " | Gal.(Med.): " << fixed << setprecision(2) << s.galMed();
+    os << " | Egz: " << s.egz()
+        << " | Gal(Vid): " << fixed << setprecision(2) << s.galVid()
+        << " | Gal(Med): " << fixed << setprecision(2) << s.galMed();
 
     return os;
 }
 
-
 Studentas Stud_iv(int budas) {
     Studentas st;
 
-    if (budas == 1 || budas == 2) {
-        cout << "Iveskite studento duomenis (Vardas Pavarde Paz1 Paz2 ... Egz): ";
-        cin >> st;  
+    if (budas == 1) {
+        cout << "Iveskite varda: ";
+        string v; cin >> v;
+        cout << "Iveskite pavarde: ";
+        string p; cin >> p;
+
+        st.setVar(v);
+        st.setPav(p);
+
+        int k;
+        cout << "Kiek pazymiu: ";
+        cin >> k;
+
+        vector<int> paz(k);
+        cout << "Iveskite pazymius: ";
+        for (int i = 0; i < k; i++)
+            cin >> paz[i];
+
+        st.setPaz(paz);
+
+        int egz;
+        cout << "Egzamino pazymys: ";
+        cin >> egz;
+        st.setEgz(egz);
+
+        st.computeGalutiniai();
     }
-    else if (budas == 3) {
-        
-        st.setVar("Vardas" + to_string(rand() % 100));
-        st.setPav("Pavarde" + to_string(rand() % 100));
+    else if (budas == 2) {
+        cout << "Iveskite varda: ";
+        string v; cin >> v;
+        cout << "Iveskite pavarde: ";
+        string p; cin >> p;
+
+        st.setVar(v);
+        st.setPav(p);
+
+        cout << "Veskite pazymius (ENTER baigia):\n";
 
         vector<int> paz;
-        int n = 3 + rand() % 8; 
-        for (int i = 0; i < n; ++i) {
-            paz.push_back(1 + rand() % 10);
+        string line;
+        getline(cin, line); 
+
+        while (true) {
+            getline(cin, line);
+            if (line.empty()) break;
+            try {
+                paz.push_back(stoi(line));
+            }
+            catch (...) {}
         }
+
         st.setPaz(paz);
-        st.setEgz(1 + rand() % 10);
+
+        int egz;
+        cout << "Egzamino pazymys: ";
+        cin >> egz;
+
+        st.setEgz(egz);
+        st.computeGalutiniai();
+    }
+    else if (budas == 3) {
+        st.setVar("Vardas" + to_string(rand() % 1000));
+        st.setPav("Pavarde" + to_string(rand() % 1000));
+
+        int k = rand() % 7 + 3; // 3–10 ND
+        vector<int> paz(k);
+        for (int i = 0; i < k; i++)
+            paz[i] = rand() % 10 + 1;
+
+        st.setPaz(paz);
+        st.setEgz(rand() % 10 + 1);
+
         st.computeGalutiniai();
     }
     else {
-       
         st.setVar("Vardas");
         st.setPav("Pavarde");
+        st.setPaz({ 1,1,1 });
         st.setEgz(1);
-        vector<int> paz = { 1, 1, 1 };
-        st.setPaz(paz);
         st.computeGalutiniai();
     }
 
--- 3 strategija/studentas.h
+++ 3 strategija/studentas.h
@@ -15,14 +15,12 @@ private:
     double galMed_{ 0.0 };
 
 public:
-    // ============= KONSTRUKTORIAI =============
-    // Default konstruktorius
     Studentas() = default;
 
-    // Konstruktorius su istream
+   
     Studentas(std::istream& is) { readStudent(is); }
 
-    // ============= RULE OF THREE =============
+
 
     // 1. Destruktorius
     ~Studentas() {
@@ -32,7 +30,7 @@ public:
         egz_ = 0;
     }
 
-    // 2. Copy konstruktorius
+    // 2. Copy 
     Studentas(const Studentas& other)
         : var_(other.var_),
         pav_(other.pav_),
@@ -42,9 +40,9 @@ public:
         galMed_(other.galMed_) {
     }
 
-    // 3. Copy assignment operatorius
+    // 3. Copy 
     Studentas& operator=(const Studentas& other) {
-        if (this != &other) {  // apsauga nuo self-assignment
+        if (this != &other) {  
             var_ = other.var_;
             pav_ = other.pav_;
             paz_ = other.paz_;
@@ -55,7 +53,6 @@ public:
         return *this;
     }
 
-    // ============= GETTERIAI =============
     inline std::string var() const { return var_; }
     inline std::string pav() const { return pav_; }
     inline const std::vector<int>& paz() const { return paz_; }
@@ -63,7 +60,6 @@ public:
     inline double galVid() const { return galVid_; }
     inline double galMed() const { return galMed_; }
 
-    // ============= SETTERIAI =============
     inline void setVar(const std::string& v) { var_ = v; }
     inline void setPav(const std::string& p) { pav_ = p; }
     inline void setPaz(const std::vector<int>& p) { paz_ = p; }
@@ -71,27 +67,20 @@ public:
     inline void setGalVid(double g) { galVid_ = g; }
     inline void setGalMed(double g) { galMed_ = g; }
 
-    // ============= PAGRINDINĖS FUNKCIJOS =============
     void computeGalutiniai();
     std::istream& readStudent(std::istream& is);
 
-    // ============= ĮVESTIES/IŠVESTIES OPERATORIAI =============
-    // Friend funkcijos leidžia pasiekti private narius
     friend std::istream& operator>>(std::istream& is, Studentas& s);
     friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
 
-    // ============= PAPILDOMI OPERATORIAI (naudingi rikiavimui) =============
-    // Palyginimo operatorius (rikiavimui pagal pavardę, tada vardą)
     bool operator<(const Studentas& other) const {
         if (pav_ != other.pav_) return pav_ < other.pav_;
         return var_ < other.var_;
     }
 
-    // Lygybės operatorius
     bool operator==(const Studentas& other) const {
         return var_ == other.var_ && pav_ == other.pav_;
     }
 };
 
-// ============= IŠORINĖS FUNKCIJOS =============
 Studentas Stud_iv(int budas);
