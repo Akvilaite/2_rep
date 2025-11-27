d285e6b3cdb402656d9e990bd8267779df8ed269
Akvilaite <akvile.beinoriute@gmail.com>
2025-11-27 17:52:51 +0200

    pakeiciu studento klase

--- 3 strategija/studentas.h
+++ 3 strategija/studentas.h
@@ -1,18 +1,14 @@
 ﻿#pragma once
-#include <string>
+#include "zmogus.h"
 #include <vector>
+#include <string>
 #include <istream>
 #include <ostream>
 #include <iomanip>
 #include <iostream>
-#include "zmogus.h"
 
-
-
-class Studentas {
+class Studentas : public Zmogus {
 private:
-    std::string var_;
-    std::string pav_;
     std::vector<int> paz_;
     int egz_{ 0 };
     double galVid_{ 0.0 };
@@ -21,15 +17,18 @@ private:
 public:
     Studentas() = default;
 
-    Studentas(std::istream& is) { readStudent(is); }
-
-
-    ~Studentas() {
+    Studentas(std::istream& is) {
+        readStudent(is);
     }
 
+    ~Studentas() {}
+
     Studentas(const Studentas& other)
-        : var_(other.var_), pav_(other.pav_), paz_(other.paz_),
-        egz_(other.egz_), galVid_(other.galVid_), galMed_(other.galMed_) {
+        : Zmogus(other.var_, other.pav_),
+        paz_(other.paz_),
+        egz_(other.egz_),
+        galVid_(other.galVid_),
+        galMed_(other.galMed_) {
     }
 
     Studentas& operator=(const Studentas& other) {
@@ -43,16 +42,12 @@ public:
         }
         return *this;
     }
-        
-    inline std::string var() const { return var_; }
-    inline std::string pav() const { return pav_; }
+
     inline const std::vector<int>& paz() const { return paz_; }
     inline int egz() const { return egz_; }
     inline double galVid() const { return galVid_; }
     inline double galMed() const { return galMed_; }
 
-    inline void setVar(const std::string& v) { var_ = v; }
-    inline void setPav(const std::string& p) { pav_ = p; }
     inline void setPaz(const std::vector<int>& p) { paz_ = p; }
     inline void setEgz(int e) { egz_ = e; }
     inline void setGalVid(double g) { galVid_ = g; }
@@ -61,8 +56,23 @@ public:
     void computeGalutiniai();
     std::istream& readStudent(std::istream& is);
 
+    void print(std::ostream& os) const override {
+        os << var_ << " " << pav_
+            << " | ND: ";
+        for (size_t i = 0; i < paz_.size(); ++i) {
+            os << paz_[i];
+            if (i < paz_.size() - 1) os << ", ";
+        }
+        os << " | Egz: " << egz_
+            << " | Gal(Vid): " << std::fixed << std::setprecision(2) << galVid_
+            << " | Gal(Med): " << std::fixed << std::setprecision(2) << galMed_;
+    }
+
     friend std::istream& operator>>(std::istream& is, Studentas& s);
-    friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& s) {
+        s.print(os);
+        return os;
+    }
 
     bool operator<(const Studentas& other) const {
         if (pav_ != other.pav_) return pav_ < other.pav_;
